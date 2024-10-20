75c10e4a9375844aec55aebbaa29d5bb4f7dadde
Drillenok <garillavovan1@gmail.com>
2024-10-20 14:37:43 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // Инициализация паралакса
     var scene = document.getElementById('intro');
     var parallaxInstance = new Parallax(scene);
+
+    // Раскрывающиеся списки
+    var coll = document.getElementsByClassName("collapsible");
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
 });
